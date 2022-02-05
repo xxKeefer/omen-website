a065533b9d79d550db27a7b734dda69c1849cf6c
xxKeefer <xxkeefer.code@gmail.com>
2022-02-06 01:33:32 +1000

    better paras

--- src/components/style/OmenMDXStyle.tsx
+++ src/components/style/OmenMDXStyle.tsx
@@ -19,16 +19,18 @@ import {
 } from '@chakra-ui/react'
 
 const OmenMDXStyle = {
-    h1: (props) => <Heading as="h1" fontSize="7xl" {...props} />,
-    h2: (props) => <Heading as="h2" fontSize="6xl" {...props} />,
-    h3: (props) => <Heading as="h3" fontSize="5xl" {...props} />,
-    h4: (props) => <Heading as="h4" fontSize="3xl" {...props} />,
-    h5: (props) => <Heading as="h5" fontSize="x-large" {...props} />,
-    h6: (props) => <Heading as="h6" fontSize="larger" {...props} />,
-    p: (props) => <Text as="p" fontSize="xl" {...props} />,
-    a: (props) => <Link as="a" fontSize="xl" color="teal.300" {...props} />,
-    pre: (props) => <Text as="pre" fontSize="xl" {...props} />,
-    code: (props) => <Code as="code" bg="teal.200" fontSize="xl" {...props} />,
+    h1: (props) => <Heading as="h1" fontSize="5xl" {...props} />,
+    h2: (props) => <Heading as="h2" fontSize="4xl" {...props} />,
+    h3: (props) => <Heading as="h3" fontSize="2xl" {...props} />,
+    h4: (props) => <Heading as="h4" fontSize="xl" {...props} />,
+    h5: (props) => <Heading as="h5" fontSize="lg" {...props} />,
+    h6: (props) => <Heading as="h6" fontSize="md" {...props} />,
+    p: (props) => (
+        <Text as="p" fontSize="lg" paddingBlock="0.5rem" {...props} />
+    ),
+    a: (props) => <Link as="a" fontSize="lg" color="teal.300" {...props} />,
+    pre: (props) => <Text as="pre" fontSize="lg" {...props} />,
+    code: (props) => <Code as="code" bg="teal.200" fontSize="lg" {...props} />,
     ul: (props) => <UnorderedList as="ul" {...props} />,
     ol: (props) => <OrderedList as="ol" {...props} />,
     li: (props) => <ListItem as="li" {...props} />,
